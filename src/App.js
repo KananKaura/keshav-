41a3e0219b6a992be9beda6e2a16489c59b3086e
KananKaura <132899568+KananKaura@users.noreply.github.com>
2024-11-07 13:43:47 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+
+import Client from './components/section/Client';
+import Working from './components/section/Working';
+import Services from './components/section/Services';
+import Testimonials from './components/section/Testimonials';
+import Portfolio from './components/section/Portfolio';
+import Architect from './components/section/Architect';
+import Specification from './components/section/Specification';
+
+function App() {
+  return (
+    <>
+     <div className="App">
+      <Router>
+      <Header />
+      <Working />
+      <Services />
+      <Portfolio/>
+      <Testimonials/>
+      <Architect/>
+      <Specification/>
+      <Routes>
+      <Route exact path='/' ></Route>
+      </Routes>
+      <Client/>
+      <Footer/>
+      </Router>
+      
+    </div>
+    </>
+  );
+}
+
+export default App;
